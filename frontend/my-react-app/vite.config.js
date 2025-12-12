fb0d5b2919fb0131f575b8383cde8c00c243b9e4
ajay <22cs10aj88@mitsgwl.acin>
2025-12-12 21:04:31 +0530

    first commit

--- frontend/my-react-app/vite.config.js
+++ frontend/my-react-app/vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Social-Agent-UI/",
+   base: '/'
 })
 
